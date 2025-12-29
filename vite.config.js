9e454399b5e8bcc136e5322ec1988f7245bd00a3
Xue Rui <2252493046@qq.com>
2025-12-29 11:03:13 +0800

    config: 配置正确的 base 路径

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // 👇 重点是这一行！必须和你 GitHub 仓库名字一模一样，前后都要有斜杠
+  base: '/-todolist-/', 
+  
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
